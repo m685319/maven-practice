2270605749198bb898d49b44c188ef4086fc2ad5
maria <mariacodes77@gmail.com>
2024-12-27 03:45:15 +0300

    added dependency on GeometryUtils module in GeometryApp

--- GeometryApp/src/main/java/org/example/Main.java
+++ GeometryApp/src/main/java/org/example/Main.java
@@ -1,10 +1,13 @@
 package org.example;
 
+import org.example.GeometryLibrary.utils.GeometryUtils;
+
 public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle(5);
         System.out.println("Circle area: " + circle.getArea());
         System.out.println("Circle perimeter: " + circle.getPerimeter());
+        System.out.println("Circle diameter in centimeters: " + GeometryUtils.metersToCentimeters(circle.getDiameter()));
 
         Rectangle rectangle = new Rectangle(4, 7);
         System.out.println("Rectangle area: " + rectangle.getArea());
