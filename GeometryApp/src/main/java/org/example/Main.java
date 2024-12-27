df1897c91908830ef52191354f0b5cc0e41f5df9
maria <mariacodes77@gmail.com>
2024-12-27 03:20:43 +0300

    created main class for GeometryApp to use GeometryLibrary

--- GeometryApp/src/main/java/org/example/Main.java
+++ GeometryApp/src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        System.out.println("Circle area: " + circle.getArea());
+        System.out.println("Circle perimeter: " + circle.getPerimeter());
 
+        Rectangle rectangle = new Rectangle(4, 7);
+        System.out.println("Rectangle area: " + rectangle.getArea());
+        System.out.println("Rectangle perimeter: " + rectangle.getPerimeter());
+
+        Triangle triangle = new Triangle(3, 4, 5);
+        System.out.println("Triangle area: " + triangle.getArea());
+        System.out.println("Triangle perimeter: " + triangle.getPerimeter());
     }
 }
